e25065efcd694db064821deb670d65c427d7dce8
Zowie van Geest <zowie@zowievangeest.nl>
2017-06-16 13:43:05 +0200

    Stock llistview in fragment

--- app/src/main/java/imtpmd/imtpmd_stoplicht/FirstFragment.java
+++ app/src/main/java/imtpmd/imtpmd_stoplicht/FirstFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -63,8 +65,24 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        String[] bijeenkomsten = {
+                "IPMEDT2",
+                "IPMEDT3",
+                "IPMEDT4"
+        };
+
+        ListView bijeenkomstenListView = (ListView) view.findViewById(R.id.bijeenkomstListView);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, bijeenkomsten);
+
+        bijeenkomstenListView.setAdapter(listViewAdapter);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
